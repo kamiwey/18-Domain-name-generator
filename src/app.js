e89bd9f5a770bb14371553980b893f6d0aa13f73
kamiwey <kamiwey@gmail.com>
2022-10-12 18:12:03 +0000

    finish

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+var ext = [".com", ".net", ".us", ".io"];
+
+// let newArray = [];
+// for (let p in pronoun)
+//   for (let e in ext)
+//     for (let a in adj)
+//       for (let n in noun) newArray.push(pronoun[p] + adj[a] + noun[n] + ext[e]);
+// console.log(newArray);
+
+for (let p = 0; p < pronoun.length; p++) {
+  for (let a = 0; a < adj.length; a++) {
+    for (let n = 0; n < noun.length; n++) {
+      for (let e = 0; e < ext.length; e++) {
+        console.log(pronoun[p] + adj[a] + noun[n] + ext[e]);
+      }
+    }
+  }
+}
